9f78c4ed6b7c3fb7e42d32464119f7ccf24014d4
Nguyen Van Minh <19130136@st.hcmuaf.edu.vn>
2024-04-13 16:14:14 +0700

    fix loi chuyen trang khi dang nhap

--- frontend/src/services/userService.js
+++ frontend/src/services/userService.js
@@ -2,7 +2,7 @@ import axios from '../axios';
 
 
 const handleLoginApi =(userEmail,userPassword) =>{
-    return axios.post('/api/login', {email: userEmail, password: userPassword} );
+    return axios.post('/api/login', {email: userEmail, password: userPassword});
 }
 
 export{handleLoginApi}
