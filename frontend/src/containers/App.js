1b0806a93a57fc3162fe93972055b3ba9e16fa43
Nguyen Van Minh <19130136@st.hcmuaf.edu.vn>
2024-05-11 16:14:23 +0700

    Phân quyền người dùng khi login

--- frontend/src/containers/App.js
+++ frontend/src/containers/App.js
@@ -16,6 +16,7 @@ import HomePage from './HomePage/HomePage';
 import CustomScrollbars from '../components/CustomScrollbars';
 import { saveDetailDoctor } from '../store/actions';
 import DetailDoctor from './Patient/Doctor/DetailDoctor';
+import Doctor from '../routes/Doctor';
 
 class App extends Component {
 
@@ -49,6 +50,7 @@ class App extends Component {
                                     <Route path={path.HOME} exact component={(Home)} />
                                     <Route path={path.LOGIN} component={userIsNotAuthenticated(Login)} />
                                     <Route path={path.SYSTEM} component={userIsAuthenticated(System)} />
+                                    <Route path={'/doctor/'} component={userIsAuthenticated(Doctor)} />
                                     <Route path={path.HOMEPAGE} component={HomePage} />
                                     <Route path={path.DETAIL_DOCTOR} component={DetailDoctor} />
                                 </Switch>
--- frontend/src/containers/Header/menuApp.js
+++ frontend/src/containers/Header/menuApp.js
@@ -1,41 +1,48 @@
 export const adminMenu = [
     { //hquản lí người dùng
-        name: 'menu.admin.manage-user', 
+        name: 'menu.admin.manage-user',
         menus: [
             {
-                name: 'menu.admin.crud',link:'/system/user-manage'},
-        
-                {
-                    name: 'menu.admin.crud-redux',link:'/system/user-redux'},
+                name: 'menu.admin.crud', link: '/system/user-manage'
+            },
+
             {
-                name: 'menu.admin.manage-doctor',link:'/system/manage-doctor'
+                name: 'menu.admin.crud-redux', link: '/system/user-redux'
+            },
+            {
+                name: 'menu.admin.manage-doctor', link: '/system/manage-doctor'
                 // subMenus: [
                 //     { name: 'menu.system.system-administrator.user-manage', link: '/system/user-manage' },
                 //     { name: 'menu.system.system-administrator.user-redux', link: '/system/user-redux' },
-                   
+
                 // ]
             },
             // { name: 'menu.system.system-parameter.header', link: '/system/system-parameter' },
-            {
-                name: 'menu.admin.manage-admin',link:'/system/user-admin'
-                // subMenus: [
-                //     { name: 'menu.system.system-administrator.user-manage', link: '/system/user-manage' },
-                //     { name: 'menu.system.system-administrator.user-redux', link: '/system/user-redux' },
-                   
-                // ]
+            //{
+            //     name: 'menu.admin.manage-admin', link: '/system/user-admin'
+            //     // subMenus: [
+            //     //     { name: 'menu.system.system-administrator.user-manage', link: '/system/user-manage' },
+            //     //     { name: 'menu.system.system-administrator.user-redux', link: '/system/user-redux' },
+
+            //     // ]
+            // },
+            { //hquản lý kế hoạch khám bệnh của bác sĩ 
+                name: 'menu.doctor.manage-schedule', link: '/doctor/manage-schedule'
+
             },
-           
-            
+
+
         ]
 
 
     },
     { //hquản lí phòng khám
-        name: 'menu.admin.clinic', 
+        name: 'menu.admin.clinic',
         menus: [
             {
-                name: 'menu.admin.manage-clinic',link:'/system/manage-clinic'},
-        
+                name: 'menu.admin.manage-clinic', link: '/system/manage-clinic'
+            },
+
             //     {
             //         name: 'menu.admin.crud-redux',link:'/system/user-redux'},
             // {
@@ -43,7 +50,7 @@ export const adminMenu = [
             //     // subMenus: [
             //     //     { name: 'menu.system.system-administrator.user-manage', link: '/system/user-manage' },
             //     //     { name: 'menu.system.system-administrator.user-redux', link: '/system/user-redux' },
-                   
+
             //     // ]
             // },
             // // { name: 'menu.system.system-parameter.header', link: '/system/system-parameter' },
@@ -52,21 +59,22 @@ export const adminMenu = [
             //     // subMenus: [
             //     //     { name: 'menu.system.system-administrator.user-manage', link: '/system/user-manage' },
             //     //     { name: 'menu.system.system-administrator.user-redux', link: '/system/user-redux' },
-                   
+
             //     // ]
             // },
-           
-            
+
+
         ]
 
 
     },
     { //hquản lí chuyên khia
-        name: 'menu.admin.specialty', 
+        name: 'menu.admin.specialty',
         menus: [
             {
-                name: 'menu.admin.manage-specialty',link:'/system/manage-specialty'},
-        
+                name: 'menu.admin.manage-specialty', link: '/system/manage-specialty'
+            },
+
             //     {
             //         name: 'menu.admin.crud-redux',link:'/system/user-redux'},
             // {
@@ -74,7 +82,7 @@ export const adminMenu = [
             //     // subMenus: [
             //     //     { name: 'menu.system.system-administrator.user-manage', link: '/system/user-manage' },
             //     //     { name: 'menu.system.system-administrator.user-redux', link: '/system/user-redux' },
-                   
+
             //     // ]
             // },
             // // { name: 'menu.system.system-parameter.header', link: '/system/system-parameter' },
@@ -83,21 +91,22 @@ export const adminMenu = [
             //     // subMenus: [
             //     //     { name: 'menu.system.system-administrator.user-manage', link: '/system/user-manage' },
             //     //     { name: 'menu.system.system-administrator.user-redux', link: '/system/user-redux' },
-                   
+
             //     // ]
             // },
-           
-            
+
+
         ]
 
 
     },
     { //hquản lí cẩm nang
-        name: 'menu.admin.handbook', 
+        name: 'menu.admin.handbook',
         menus: [
             {
-                name: 'menu.admin.manage-handbook',link:'/system/manage-handbook'},
-        
+                name: 'menu.admin.manage-handbook', link: '/system/manage-handbook'
+            },
+
             //     {
             //         name: 'menu.admin.crud-redux',link:'/system/user-redux'},
             // {
@@ -105,7 +114,7 @@ export const adminMenu = [
             //     // subMenus: [
             //     //     { name: 'menu.system.system-administrator.user-manage', link: '/system/user-manage' },
             //     //     { name: 'menu.system.system-administrator.user-redux', link: '/system/user-redux' },
-                   
+
             //     // ]
             // },
             // // { name: 'menu.system.system-parameter.header', link: '/system/system-parameter' },
@@ -114,14 +123,27 @@ export const adminMenu = [
             //     // subMenus: [
             //     //     { name: 'menu.system.system-administrator.user-manage', link: '/system/user-manage' },
             //     //     { name: 'menu.system.system-administrator.user-redux', link: '/system/user-redux' },
-                   
+
             //     // ]
             // },
-           
-            
+
+
         ]
 
 
     },
-    
+
+];
+
+export const doctorMenu = [
+    {
+        name: 'menu.admin.manage-user',
+        menus: [
+
+            { //hquản lý kế hoạch khám bệnh
+                name: 'menu.doctor.manage-schedule', link: '/doctor/manage-schedule'
+            },
+        ]
+    }
+
 ];
